2c4f27e860760d09391e4ee0f34a242eb286dcec
shruti0320 <98652188+shruti0320@users.noreply.github.com>
2022-06-30 11:01:57 +0530

    BG updated

--- PP project.py
+++ PP project.py
@@ -1,22 +1,46 @@
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
+import time
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
 detector = HandDetector(maxHands=1)
-
+timer=0
+stateResult=False
+startGame=False
 # Loop until you hit the Esc key
 while True:
     success, img = cap.read()
     imgBG = cv2.imread('imgBG.png')
-    imgScaled = cv2.resize(img, (0, 0), None, 0.495, 0.635)
+    imgScaled = cv2.resize(img, (0, 0), None, 0.463, 0.685)
+   
+    # Find Hands
+    hands, img = detector.findHands(imgScaled)  # with draw
 
-    cv2.imshow("Image", img)
+    if startGame:
+
+        if stateResult is False:
+            timer = time.time()- intialTime
+            cv2.putText(imgBG, str(int(timer)),(542,390),cv2.FONT_HERSHEY_PLAIN,6,(255,0,255),4)
+    if hands:
+        hand = hands[0]
+        fingers= detector.fingersUp(hand)
+        print(fingers)
+
+    imgBG[157:486, 584:880] = imgScaled
+
+    #cv2.imshow("Image", img)
     cv2.imshow("BG", imgBG)
-    cv2.imshow("Scaled", imgScaled)
+    #cv2.imshow("Scaled", imgScaled)
+
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        startGame=True
+        initialTime = time.time()    
 
 
 # Detect if the Esc key has been pressed
@@ -24,8 +48,4 @@ while True:
     if c == 27:
         break
 
-# Find Hands
-    hands, img = detector.findHands(img)  # with draw
 
-    if hands:
-        hand = hands[0]
